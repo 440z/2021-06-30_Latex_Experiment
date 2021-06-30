9577667ff9f78c69070acee9ed78a880a5dcb07f
JonasParringer <jonasparringer@gmx.de>
2021-06-30 15:45:33 +0200

    gepushed fuer zu Hause

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    //let x, y, z;  // Statement 1
+    //x = 5;        // Statement 2
+    //y = 6;        // Statement 3
+    //z = x + y;    // Statement 4
+
+    //alert(z);
+
+    //document.getElementById("demo").innerHTML =
+    //"The value of z is " + z + ".";
+
+    //alert("test");
+
+
+
+    //alert("test");
+
+
+htlatex("C: \Users\ITA8 - TN04\OneDrive - IT - Akademie Dr.Heuer GmbH\git\2021 - 06 -30_Latex_Experiment\files\LatexBeispielFile.tex");
+        

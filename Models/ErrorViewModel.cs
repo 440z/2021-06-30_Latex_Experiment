146a309221ecc6a466af287372d9cf3aece8e4c3
JonasParringer <jonasparringer@gmx.de>
2021-06-30 12:06:59 +0200

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_06_30_Latex_Experiment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
